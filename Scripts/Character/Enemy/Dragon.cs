6686d24dfa203301e892096b1e29412b76bfbfcd
wxsan999-sketch <wxsan999@gmail.com>
2025-11-21 01:35:42 +0700

    Update Dragon.cs

--- Scripts/Character/Enemy/Dragon.cs
+++ Scripts/Character/Enemy/Dragon.cs
@@ -1,6 +1,44 @@
 using UnityEngine;
 
-public class Dragon
+public class Dragon : Enemy
 {
-    
+   
+    public float BreathAttackRange = 10f;   
+    public float MeleeAttackRange = 2f;     
+
+   
+    public override void Attack(Player player)
+    {
+        if (player == null) return;
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        
+        if (distance <= MeleeAttackRange)
+        {
+            Debug.Log("MeleeAttackRange!");
+            player.TakeDamage(AttackDamage);
+        }
+
+        
+        else if (distance <= BreathAttackRange)
+        {
+            FireBreath(player);
+        }
+
+       
+        else
+        {
+            Debug.Log();
+        }
+    }
+
+   
+    public void FireBreath(Player player)
+    {
+        float fireDamage = AttackDamage * 2f;
+
+        Debug.Log($"BreathAttackRange{fireDamage}");
+        player.TakeDamage(fireDamage);
+    }
 }
